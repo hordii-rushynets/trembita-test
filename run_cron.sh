fddfbce9a006bf8088e996056ef7bc80b6308266
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-11-11 13:28:44 +0200

    uncomment lines to run task each quarter

--- run_cron.sh
+++ run_cron.sh
@@ -4,8 +4,8 @@ echo "cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRE
 
 # echo "*/5 * * * * cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} python3 main.py >> /var/log/trembita.log 2>&1" > /etc/cron.d/trembita
 
-# echo "59 23 31 3,12 * cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} python3 main.py >> /var/log/trembita.log 2>&1" > /etc/cron.d/trembita
-# echo "59 23 30 6,9 * cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} python3 main.py >> /var/log/trembita.log 2>&1" > /etc/cron.d/trembita
+echo "59 23 31 3,12 * cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} python3 main.py >> /var/log/trembita.log 2>&1" > /etc/cron.d/trembita
+echo "59 23 30 6,9 * cd /app && DIIA_API_URL=${DIIA_API_URL} POSTGRES_DB=${POSTGRES_DB} POSTGRES_USER=${POSTGRES_USER} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} python3 main.py >> /var/log/trembita.log 2>&1" > /etc/cron.d/trembita
 
 chmod 0644 /etc/cron.d/trembita
 crontab /etc/cron.d/trembita
